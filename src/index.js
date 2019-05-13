77b40caf4184bfa8d351b783082f5a88a992aaaa
Sarah Maris <sarah@sarahmaris.com>
2019-05-13 12:11:45 -0400

    Add store and Provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reducers from './reducers';
+import middleware from './middleware';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create store with reducers and middleware
+const store = createStore(reducers, middleware);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
